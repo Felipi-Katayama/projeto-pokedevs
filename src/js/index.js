b58cbe8685d3d2aff3052e0680faf83b41ae0d99
Felipi-Katayama <111012586+Felipi-Katayama@users.noreply.github.com>
2024-10-13 19:32:40 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+//Passo 1 - Precisamos criar uma variável no JS para trabalhar com a listagem de pokedevs
+const listaSelecaoPokedevs = document.querySelectorAll(".pokedev");
+
+//Passo 2 - Identificar o evento de clique no elemento da listagem
+listaSelecaoPokedevs.forEach(pokedev => {
+    pokedev.addEventListener("click", () => {
+        esconderCartaoPokedev();
+
+        const idPokedevSelecionado = mostrarCartaoPokedevSelecionado(pokedev);
+
+        desativarPokedevNaListagem();
+        
+        ativarPokedevSelecionadoNaListagem(idPokedevSelecionado);
+    })
+})
+
+function ativarPokedevSelecionadoNaListagem(idPokedevSelecionado) {
+    const pokedevSelecionadoNaListagem = document.getElementById(idPokedevSelecionado);
+    pokedevSelecionadoNaListagem.classList.add("ativo");
+}
+
+function desativarPokedevNaListagem() {
+    const pokedevAtivoNaListagem = document.querySelector(".ativo");
+    pokedevAtivoNaListagem.classList.remove("ativo");
+}
+
+function mostrarCartaoPokedevSelecionado(pokedev) {
+    const idPokedevSelecionado = pokedev.attributes.id.value;
+    const idDoCartaoPokedevParaAbrir = "cartao-" + idPokedevSelecionado;
+    const CartaoPokedevParaAbrir = document.getElementById(idDoCartaoPokedevParaAbrir);
+    CartaoPokedevParaAbrir.classList.add("aberto");
+    return idPokedevSelecionado;
+}
+
+function esconderCartaoPokedev() {
+    const cartaoPokedevAberto = document.querySelector(".aberto");
+    cartaoPokedevAberto.classList.remove("aberto");
+}
